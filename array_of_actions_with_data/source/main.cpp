8075a0411b6b72f6c97a583a9d90dcafd1518d00
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2020-01-22 23:20:10 +0800

    implemented array of actions with data

--- array_of_actions_with_data/source/main.cpp
+++ array_of_actions_with_data/source/main.cpp
@@ -15,275 +15,426 @@
 #include <variant>
 
 /**
- * Best way to represent an array of actions
+ * Best way to represent an array of actions with data
  **/
 
-using data_type = int;
-
-enum class test_type
+namespace test
 {
-	enums,
-	function_pointers,
-	polymorphic_objects,
-	std_function,
-	std_function_containing_lambdas,
-	std_variant
-};
-
-enum class action
-{
-	multiply,
-	divide,
-	total_actions
-};
-
-using action_function = data_type(data_type data1, data_type data2);
-
-auto multiply(data_type data1, data_type data2)
-{
-	return data1 * data2;
-};
-
-auto divide(data_type data1, data_type data2)
-{
-	return data1 / data2;
-};
-
-class action_class
-{
-public:
-	action_class() = default;
-	virtual ~action_class() = default;
-
-	virtual data_type operator()(data_type data1, data_type data2) const = 0;
-};
-
-class multiply_action_class : public action_class
-{
-public:
-	multiply_action_class() = default;
-
-	data_type operator()(data_type data1, data_type data2) const override
+	enum class test_type
 	{
-		return multiply(data1, data2);
-	}
-};
+		enums,
+		function_pointers,
+		polymorphic_objects,
+		std_function,
+		std_variant
+	};
 
-class divide_action_class : public action_class
-{
-public:
-	divide_action_class() = default;
-
-	data_type operator()(data_type data1, data_type data2) const override
+	struct character
 	{
-		return divide(data1, data2);
-	}
-};
+		float health;
+		float attack;
+	};
 
-constexpr auto multiply_lambda = [](data_type data1, data_type data2)
-{
-	return multiply(data1, data2);
-};
-
-constexpr auto divide_lambda = [](data_type data1, data_type data2)
-{
-	return divide(data1, data2);
-};
-
-using std_action_function = std::function<action_function>;
-
-using action_variant = std::variant<decltype(multiply_lambda), decltype(divide_lambda)>;
-
-void test(int num_arguments, char *arguments[])
-{
-	auto generator = [](auto begin, auto end)
+	enum class action_type
 	{
-		return
-		[
-			rng_engine = std::mt19937_64{957546},
-			rng_range = std::uniform_int_distribution{begin, end}
-		]() mutable
+		damage,
+		heal,
+		buff,
+		total_actions
+	};
+
+	struct action
+	{
+		action_type type;
+		void* data;
+	};
+
+	using action_function = character(character subject, void* data);
+
+	struct action_function_data
+	{
+		action_function* function;
+		void* data;
+	};
+
+	auto damage(character subject, void* data)
+	{
+		return character{subject.health - *static_cast<float*>(data), subject.attack};
+	};
+
+	auto heal(character subject, void* data)
+	{
+		return character{subject.health + *static_cast<float*>(data), subject.attack};
+	};
+
+	auto buff(character subject, void* data)
+	{
+		return character{subject.health, subject.attack + *static_cast<float*>(data)};
+	};
+
+	class action_class
+	{
+	public:
+		action_class() = default;
+		virtual ~action_class() = default;
+
+		virtual character operator()(character subject) const = 0;
+	};
+
+	class damage_action_class : public action_class
+	{
+	public:
+		damage_action_class() = default;
+		damage_action_class(const float damage)
+		:
+		damage{damage}
 		{
-			return rng_range(rng_engine);
+
+		};
+
+		character operator()(character subject) const override
+		{
+			return character{subject.health - damage, subject.attack};
+		}
+
+	private:
+		float damage;
+	};
+
+	class heal_action_class : public action_class
+	{
+	public:
+		heal_action_class() = default;
+		heal_action_class(const float heal)
+		:
+		heal{heal}
+		{
+
+		};
+
+		character operator()(character subject) const override
+		{
+			return character{subject.health + heal, subject.attack};
+		}
+
+	private:
+		float heal;
+	};
+
+	class buff_action_class : public action_class
+	{
+	public:
+		buff_action_class() = default;
+		buff_action_class(const float buff)
+		:
+		buff{buff}
+		{
+
+		};
+
+		character operator()(character subject) const override
+		{
+			return character{subject.health, subject.attack + buff};
+		}
+
+	private:
+		float buff;
+	};
+
+	constexpr auto damage_variant = [](const auto damage)
+	{
+		return [damage](character subject)
+		{
+			return character{subject.health - damage, subject.attack};
 		};
 	};
 
-	auto data_generator = generator(1, 10'000);
-
-	std::cout << "Array of actions test\n";
-
-	// const auto chosen_test_type = test_type::std_variant;
-	// const auto num_data = 100'000u;
-
-	if(num_arguments < 3)
+	constexpr auto heal_variant = [](const auto heal)
 	{
-		throw std::invalid_argument{"Not enough arguments lol"};
-	}
+		return [heal](character subject)
+		{
+			return character{subject.health + heal, subject.attack};
+		};
+	};
 
-	const auto chosen_test_type = static_cast<test_type>(std::stoull(arguments[1]));
-	const auto num_data = std::stoull(arguments[2]);
-
-	auto data1_array = std::vector<data_type>(num_data);
-	auto data2_array = std::vector<data_type>(num_data);
-	auto results = std::vector<data_type>(num_data);
-
-	std::generate(data1_array.begin(), data1_array.end(), data_generator);
-	std::generate(data2_array.begin(), data2_array.end(), data_generator);
-
-	auto start = std::chrono::steady_clock::now();
-	auto end = start;
-
-	if(chosen_test_type == test_type::enums)
+	constexpr auto buff_variant = [](const auto buff)
 	{
-		std::cout << "Testing for enums...\n";
-
-		auto actions = std::vector<action>(num_data);
-		std::generate(actions.begin(), actions.end(), [action_generator = generator(std::size_t{}, static_cast<std::size_t>(action::total_actions) - 1)]() mutable
+		return [buff](character subject)
 		{
-			return static_cast<action>(action_generator());
-		});
+			return character{subject.health, subject.attack + buff};
+		};
+	};
 
-		start = std::chrono::steady_clock::now();
-		for(auto index = std::size_t{}, size = data1_array.size(); index < size; ++index)
+	using std_action_function = std::function<character(character subject)>;
+
+	using action_variant = std::variant<decltype(damage_variant(float{})), decltype(heal_variant(float{})), decltype(buff_variant(float{}))>;
+
+	void start_test(int num_arguments, char *arguments[])
+	{
+		auto generator = [](auto begin, auto end)
 		{
-			if(actions[index] == action::multiply)
+			const auto seed = 957546;
+
+			if constexpr(std::is_floating_point_v<decltype(begin)>)
 			{
-				results[index] = multiply(data1_array[index], data2_array[index]);
-			}
-			else if(actions[index] == action::divide)
-			{
-				results[index] = divide(data1_array[index], data2_array[index]);
+				return
+				[
+					rng_engine = std::mt19937_64{seed},
+					rng_range = std::uniform_real_distribution{begin, end}
+				]() mutable
+				{
+					return rng_range(rng_engine);
+				};
 			}
 			else
 			{
-				throw std::runtime_error{"Generated an invalid action!"};
+				return
+				[
+					rng_engine = std::mt19937_64{seed},
+					rng_range = std::uniform_int_distribution{begin, end}
+				]() mutable
+				{
+					return rng_range(rng_engine);
+				};
+			}
+		};
+
+		auto data_generator = generator(1.0f, 10'000.0f);
+
+		std::cout << "Array of actions with data test\n";
+
+		// const auto chosen_test_type = test_type::function_pointers;
+		// const auto num_data = 1'000'000u;
+
+		if(num_arguments < 3)
+		{
+			throw std::invalid_argument{"Not enough arguments lol"};
+		}
+
+		const auto chosen_test_type = static_cast<test_type>(std::stoull(arguments[1]));
+		const auto num_data = std::stoull(arguments[2]);
+
+		auto characters = std::vector<character>(num_data);
+		auto results = std::vector<character>(num_data);
+
+		std::generate(characters.begin(), characters.end(), [&data_generator]() mutable
+		{
+			return character{data_generator(), data_generator()};
+		});
+
+		auto start = std::chrono::steady_clock::now();
+		auto end = start;
+
+		if(chosen_test_type == test_type::enums)
+		{
+			std::cout << "Testing for enums...\n";
+
+			auto actions = std::vector<action>();
+			std::generate_n(std::back_inserter(actions), num_data, [&data_generator, action_generator = generator(std::size_t{}, static_cast<std::size_t>(action_type::total_actions) - 1)]() mutable
+			{
+				return action{static_cast<action_type>(action_generator()), new (std::malloc(sizeof(float))) float(data_generator())};
+			});
+
+			start = std::chrono::steady_clock::now();
+			for(auto index = std::size_t{}, size = characters.size(); index < size; ++index)
+			{
+				if(actions[index].type == action_type::buff)
+				{
+					results[index] = character{characters[index].health - *static_cast<float*>(actions[index].data), characters[index].attack};
+				}
+				else if(actions[index].type == action_type::damage)
+				{
+					results[index] = character{characters[index].health + *static_cast<float*>(actions[index].data), characters[index].attack};
+				}
+				else if(actions[index].type == action_type::heal)
+				{
+					results[index] = character{characters[index].health, characters[index].attack + *static_cast<float*>(actions[index].data)};
+				}
+				else
+				{
+					throw std::runtime_error{"Generated an invalid action!"};
+				}
+			}
+			end = std::chrono::steady_clock::now();
+
+			for(auto& current_action : actions)
+			{
+				std::free(current_action.data);
 			}
 		}
-		end = std::chrono::steady_clock::now();
-	}
-	else if(chosen_test_type == test_type::function_pointers)
-	{
-		std::cout << "Testing for function pointers...\n";
-
-		auto actions = std::vector<action_function*>(num_data);
-
-		auto choices = std::array{multiply, divide};
-
-		std::generate(actions.begin(), actions.end(), [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
+		else if(chosen_test_type == test_type::function_pointers)
 		{
-			return choices[action_generator()];
-		});
-		
-		start = std::chrono::steady_clock::now();
-		for(auto index = std::size_t{}, size = data1_array.size(); index < size; ++index)
-		{
-			results[index] = actions[index](data1_array[index], data2_array[index]);
-		}
-		end = std::chrono::steady_clock::now();
-	}
-	else if(chosen_test_type == test_type::polymorphic_objects)
-	{
-		std::cout << "Testing for polymorphic objects...\n";
+			std::cout << "Testing for function pointers...\n";
 
-		const auto multiply_action_object = multiply_action_class{};
-		const auto divide_action_object = divide_action_class{};
+			auto actions = std::vector<action_function_data>();
 
-		auto actions = std::vector<const action_class*>(num_data);
-
-		auto choices = std::array{static_cast<const action_class*>(&multiply_action_object), static_cast<const action_class*>(&divide_action_object)};
-
-		std::generate(actions.begin(), actions.end(), [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
-		{
-			return choices[action_generator()];
-		});
-		
-		start = std::chrono::steady_clock::now();
-		for(auto index = std::size_t{}, size = data1_array.size(); index < size; ++index)
-		{
-			results[index] = (*actions[index])(data1_array[index], data2_array[index]);
-		}
-		end = std::chrono::steady_clock::now();
-	}
-	else if(chosen_test_type == test_type::std_function)
-	{
-		std::cout << "Testing for standard functions...\n";
-
-		auto actions = std::vector<std_action_function>(num_data);
-
-		auto choices = std::array{std_action_function{divide}, std_action_function{multiply}};
-
-		std::generate(actions.begin(), actions.end(), [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
-		{
-			return choices[action_generator()];
-		});
-		
-		start = std::chrono::steady_clock::now();
-		for(auto index = std::size_t{}, size = data1_array.size(); index < size; ++index)
-		{
-			results[index] = actions[index](data1_array[index], data2_array[index]);
-		}
-		end = std::chrono::steady_clock::now();
-	}
-	else if(chosen_test_type == test_type::std_function_containing_lambdas)
-	{
-		std::cout << "Testing for standard functions containing lambdas...\n";
-
-		auto actions = std::vector<std_action_function>(num_data);
-
-		auto choices = std::array{std_action_function{divide_lambda}, std_action_function{multiply_lambda}};
-
-		std::generate(actions.begin(), actions.end(), [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
-		{
-			return choices[action_generator()];
-		});
-		
-		start = std::chrono::steady_clock::now();
-		for(auto index = std::size_t{}, size = data1_array.size(); index < size; ++index)
-		{
-			results[index] = actions[index](data1_array[index], data2_array[index]);
-		}
-		end = std::chrono::steady_clock::now();
-	}
-	else if(chosen_test_type == test_type::std_variant)
-	{
-		std::cout << "Testing for standard variant...\n";
-
-		auto actions = std::vector<action_variant>{};
-
-		auto choices = std::array{action_variant{divide_lambda}, action_variant{multiply_lambda}};
-
-		std::generate_n(std::back_inserter(actions), num_data, [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
-		{
-			return choices[action_generator()];
-		});
-		
-		start = std::chrono::steady_clock::now();
-		for(auto index = std::size_t{}, size = data1_array.size(); index < size; ++index)
-		{
-			const auto data1 = data1_array[index];
-			const auto data2 = data2_array[index];
-			results[index] = std::visit([data1, data2](auto function)
+			using choice_generator = std::function<action_function_data()>;
+			auto choices = std::array
 			{
-				return function(data1, data2);
-			}, actions[index]);
-		}
-		end = std::chrono::steady_clock::now();
-	}
-	else
-	{
-		throw std::invalid_argument{"invalid test type lol"};
-	}
-	
-	const auto duration = end - start;
+				choice_generator{[&data_generator]
+				{
+					return action_function_data{damage, new (std::malloc(sizeof(float))) float(data_generator())};
+				}},
+				choice_generator{[&data_generator]
+				{
+					return action_function_data{heal, new (std::malloc(sizeof(float))) float(data_generator())};
+				}},
+				choice_generator{[&data_generator]
+				{
+					return action_function_data{buff, new (std::malloc(sizeof(float))) float(data_generator())};
+				}}
+			};
 
-	std::cout << "Completed in " << duration.count() << "ns\n";
+			std::generate_n(std::back_inserter(actions), num_data, [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
+			{
+				return choices[action_generator()]();
+			});
+			
+			start = std::chrono::steady_clock::now();
+			for(auto index = std::size_t{}, size = characters.size(); index < size; ++index)
+			{
+				results[index] = actions[index].function(characters[index], actions[index].data);
+			}
+			end = std::chrono::steady_clock::now();
+
+			for(auto& current_action : actions)
+			{
+				std::free(current_action.data);
+			}
+		}
+		else if(chosen_test_type == test_type::polymorphic_objects)
+		{
+			std::cout << "Testing for polymorphic objects...\n";
+
+			auto actions = std::vector<std::unique_ptr<action_class>>();
+
+			using choice_generator = std::function<std::unique_ptr<action_class>()>;
+			auto choices = std::array
+			{
+				choice_generator{[&data_generator]
+				{
+					return std::make_unique<damage_action_class>(damage_action_class{data_generator()});
+				}},
+				choice_generator{[&data_generator]
+				{
+					return std::make_unique<heal_action_class>(heal_action_class{data_generator()});
+				}},
+				choice_generator{[&data_generator]
+				{
+					return std::make_unique<buff_action_class>(buff_action_class{data_generator()});
+				}}
+			};
+
+			std::generate_n(std::back_inserter(actions), num_data, [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
+			{
+				return choices[action_generator()]();
+			});
+			
+			start = std::chrono::steady_clock::now();
+			for(auto index = std::size_t{}, size = characters.size(); index < size; ++index)
+			{
+				results[index] = (*actions[index])(characters[index]);
+			}
+			end = std::chrono::steady_clock::now();
+		}
+		else if(chosen_test_type == test_type::std_function)
+		{
+			std::cout << "Testing for standard functions...\n";
+
+			auto actions = std::vector<std_action_function>(num_data);
+
+			using choice_generator = std::function<std_action_function()>;
+			auto choices = std::array
+			{
+				choice_generator{[&data_generator]
+				{
+					return [damage = data_generator()](auto subject)
+					{
+						return character{subject.health - damage, subject.attack}; 
+					};
+				}},
+				choice_generator{[&data_generator]
+				{
+					return [heal = data_generator()](auto subject)
+					{
+						return character{subject.health + heal, subject.attack}; 
+					};
+				}},
+				choice_generator{[&data_generator]
+				{
+					return [buff = data_generator()](auto subject)
+					{
+						return character{subject.health, subject.attack + buff}; 
+					};
+				}}
+			};
+
+			std::generate(actions.begin(), actions.end(), [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
+			{
+				return choices[action_generator()]();
+			});
+			
+			start = std::chrono::steady_clock::now();
+			for(auto index = std::size_t{}, size = characters.size(); index < size; ++index)
+			{
+				results[index] = actions[index](characters[index]);
+			}
+			end = std::chrono::steady_clock::now();
+		}
+		else if(chosen_test_type == test_type::std_variant)
+		{
+			std::cout << "Testing for standard variant...\n";
+
+			auto actions = std::vector<action_variant>();
+
+			using choice_generator = std::function<action_variant()>;
+			auto choices = std::array
+			{
+				choice_generator{[&data_generator]
+				{
+					return damage_variant(data_generator());
+				}},
+				choice_generator{[&data_generator]
+				{
+					return heal_variant(data_generator());
+				}},
+				choice_generator{[&data_generator]
+				{
+					return buff_variant(data_generator());
+				}}
+			};
+
+			std::generate_n(std::back_inserter(actions), num_data, [choices, action_generator = generator(std::size_t{}, std::size(choices) - 1)]() mutable
+			{
+				return choices[action_generator()]();
+			});
+			
+			start = std::chrono::steady_clock::now();
+			for(auto index = std::size_t{}, size = characters.size(); index < size; ++index)
+			{
+				results[index] = std::visit([subject = characters[index]](auto chosen_action)
+				{
+					return chosen_action(subject);
+				}, actions[index]);
+			}
+			end = std::chrono::steady_clock::now();
+		}
+		else
+		{
+			throw std::invalid_argument{"invalid test type lol"};
+		}
+		
+		const auto duration = end - start;
+
+		std::cout << "Completed in " << duration.count() << "ns\n";
+	}
 }
 
 int main(int num_arguments, char *arguments[])
 {
 	try
 	{
-		test(num_arguments, arguments);
+		test::start_test(num_arguments, arguments);
 		return EXIT_SUCCESS;
 	}
 	catch(const std::exception& e)
@@ -291,4 +442,4 @@ int main(int num_arguments, char *arguments[])
 		std::cerr << e.what() << '\n';
 		return EXIT_FAILURE;
 	}
-}
+};
